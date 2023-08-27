bfaf099a5ac6ff7e8209314cbcef0424744e5f00
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-08-27 22:57:33 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {23, 45, 1, 2, 4, 67, 5, 6, -66};
+        int target =45;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+
+    }
+
+    static int linearSearch(int[] arr, int target)
+    {
+        if (arr.length == 0)
+        {
+            return -1;
+        }
+        for (int index = 0; index < arr.length; index++)
+        {
+            int element = arr[index];
+            if (element == target)
+            {
+                return element;
+            }
+        }
+        return -1;
+    }
+
+}
